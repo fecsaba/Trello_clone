730187c778fa3d4389963f024ec2e6d586d7a02f
fcs999 <fcs999@gmail.com>
2018-11-24 01:16:59 +0100

    Refresh Category

--- database/migrations/2018_11_23_232644_create_categories_table.php
+++ database/migrations/2018_11_23_232644_create_categories_table.php
@@ -1,31 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateCategoriesTable extends Migration
     {
-        Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+        public function up()
+        {
+            Schema::create('categories', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('categories');
+        public function down()
+        {
+            Schema::dropIfExists('categories');
+        }
     }
-}
