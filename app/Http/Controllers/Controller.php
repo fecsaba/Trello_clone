e9307026f7465569f0ada90d82ee504f4c6058b5
fcs999 <fcs999@gmail.com>
2018-11-24 02:09:06 +0100

    Modified and composer require laravel/passport

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    // use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'name' => $request->name,
+            'category_id' => $request->category_id,
+            'user_id' => $request->user_id,
+            'order' => $request->order
+        ]);
+
+        $data = [
+            'data' => $task,
+            'status' => (bool) $task,
+            'message' => $task ? 'Task Created!' : 'Error Creating Task',
+        ];
+
+        return response()->json($data);
+    }
 }
