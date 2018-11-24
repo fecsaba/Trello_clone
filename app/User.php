b4f89f1bed4122108688851c5f4728bb4e20b18e
fcs999 <fcs999@gmail.com>
2018-11-24 02:16:05 +0100

    call the Passport::routes method

--- app/User.php
+++ app/User.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use SoftDeletes, Notifiable;
+    use HasApiTokens, SoftDeletes, Notifiable;
 
     protected $fillable = ['name', 'email', 'password'];
 
