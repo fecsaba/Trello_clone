9bf633b31bdd7b7ff155e1b77a10974971ceeb70
fcs999 <fcs999@gmail.com>
2018-11-24 01:19:38 +0100

    refresh task migration

--- database/migrations/2018_11_23_232305_create_tasks_table.php
+++ database/migrations/2018_11_23_232305_create_tasks_table.php
@@ -1,31 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Database\Migrations\Migration;
 
-class CreateTasksTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateTasksTable extends Migration
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
-        });
-    }
+        public function up()
+        {
+            Schema::create('tasks', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->unsignedInteger('category_id');
+                $table->unsignedInteger('user_id');
+                $table->integer('order');
+                $table->timestamps();
+                $table->softDeletes();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('tasks');
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('category_id')->references('id')->on('categories');
+            });
+        }
+
+        public function down()
+        {
+            Schema::dropIfExists('tasks');
+        }
     }
-}
