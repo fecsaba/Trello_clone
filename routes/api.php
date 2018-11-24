87fdbe48e0c7c53b7b5b9c20c15cb2bf7a000d83
fcs999 <fcs999@gmail.com>
2018-11-24 02:25:29 +0100

    Modified api.php

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
     Route::resource('/task', 'TaskController');
     Route::get('/category/{category}/tasks', 'CategoryController@tasks');
     Route::resource('/category', 'CategoryController');
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
